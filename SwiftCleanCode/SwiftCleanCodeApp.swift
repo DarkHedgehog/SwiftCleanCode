96b57750456b2ca06340b54afb2f6848579ea180
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-02-09 20:01:01 +0300

    Initial Commit

---
+++ SwiftCleanCode/SwiftCleanCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCleanCodeApp.swift
+//  SwiftCleanCode
+//
+//  Created by Aleksandr Derevenskih on 09.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCleanCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
