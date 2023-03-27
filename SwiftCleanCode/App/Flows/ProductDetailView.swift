b20f23a3590125b0f98e4de3a12dbb836f359d04
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-03-27 13:29:28 +0300

    add productDetailView

---
+++ SwiftCleanCode/App/Flows/ProductDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetail.swift
+//  SwiftCleanCode
+//
+//  Created by Aleksandr Derevenskih on 24.03.2023.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+    }
+}
